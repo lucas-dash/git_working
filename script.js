f949471a52b440ec1161dffb67c932f227cd83a3
lucas-dash <homolka.lukas10@gmail.com>
2022-12-08 21:15:07 +0100

    add typescript

---
+++ script.js
@@ -0,0 +1,4 @@
+var current = function (num, num2) {
+    return num + num2;
+};
+current(2, 2);
---
+++ script.ts
@@ -0,0 +1,5 @@
+let current = (num:number, num2:number): number => {
+    return num + num2
+}
+
+current(2,2)
